df5dfea95d46f899e53e59d17c9d3da202b816e8
Delia Bouhan <deliabouhan10@gmail.com>
2019-09-22 20:56:45 -0500

    finished hangman game

---
+++ assets/javascript/game.js
@@ -0,0 +1,96 @@
+
+var breeds = [
+    "labrador",
+    "malamute",
+    "husky",
+    "doberman",
+    "rottweiler",
+    "beagle",
+    "pug",
+    "chihuahua",
+    "newfoundland",
+    "schnauzer"
+];
+var isGameStarted = false;
+var isGameOver = false;
+var wins = 0;
+var losses = 0;
+var word = ""
+var guesses = 0;
+var answerArray = []
+var guessesText = document.getElementById("lettersGuess");
+
+
+var wordDisplay = document.getElementById("underscore");
+var header = document.getElementById("header");
+var winsTotal = document.getElementById("wins");
+var lossTotal = document.getElementById("losses");
+var newHeader = "Guess the breed of dog!";
+document.onkeyup = function () {
+    if (isGameStarted === false) {
+        initializeGame();
+    } else if (isGameStarted === true && isGameOver === false) {
+        var userGuess = event.key;
+        if(word.indexOf(userGuess) !== -1) {
+            for (var j = 0; j < word.length; j++) {
+                if (word[j] === userGuess) {
+                    answerArray[j] = userGuess;
+                } 
+            }
+        }else {
+            guesses--;
+            guessesText.textContent = "Guesses Remaining: " + guesses;
+            
+        }
+        wordDisplay.textContent = (answerArray.join(" "));
+        if (answerArray.indexOf("_") === -1) {
+            isGameOver = true
+            won();
+            
+        }
+        if (guesses === 0) {
+            wordDisplay.textContent = word;
+            lost();
+            
+        }
+        
+    } else if (isGameStarted === true && isGameOver === true) {
+        //restart
+        //initializeGame();
+        isGameStarted = true;
+        isGameOver = false
+        wordDisplay.textContent = (answerArray.join(" "));
+    }
+};
+
+var won = function () {
+    wins++
+    header.textContent = "Press Any Key To Play Again";
+    winsTotal.textContent = "Wins: " + wins;
+    isGameStarted = false;
+    word = "";
+    answerArray = [];
+    alert("You won!");
+}
+var lost = function () {
+    losses++
+    header.textContent = "Press Any Key To Play Again";
+    lossTotal.textContent = "Losses: " + losses;
+    isGameStarted = false;
+    word = "";
+    answerArray = [];
+    alert("Aw, better luck next time!")
+}
+
+var initializeGame = function () {
+    word = breeds[Math.floor(Math.random() * breeds.length)];
+    guesses = word.length;
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    };
+    wordDisplay.textContent = (answerArray.join(" "));
+    guessesText.textContent = "Guesses Remaining: " + guesses;
+    header.textContent = newHeader;
+    isGameStarted = true;
+}
+
